6cf46f946edbe21bd9b4e63786d5fb3c4eeae38c
José Rojas <joseantoniorojasl02@gmail.com>
2024-01-30 17:10:22 -0400

    🔧 Fixed import QuantitySelector

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { QuantitySelector, Title } from '@/components';
+import { Title } from '@/components';
 import { initialData } from '@/seed/seed';
 
 const productsInCart = [
