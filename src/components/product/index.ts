c0255ec8ef760dbf57ddccd6c952864a8aeabe49
José Rojas <joseantoniorojasl02@gmail.com>
2024-01-30 13:51:18 -0400

    🏙️ Added product screen

---
+++ src/components/product/index.ts
@@ -0,0 +1,4 @@
+export { QuantitySelector } from './quantity-selector/QuantitySelector';
+export { SizeSelector } from './size-selector/SizeSelector';
+export { ProductSlideShow } from './slideshow/ProductSlideShow';
+export { ProductMobileSlideShow } from './slideshow/ProductMobileSlideShow';
