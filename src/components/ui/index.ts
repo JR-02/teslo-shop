d7e7b9470b7013a08fe6da691455b80c10222c95
José Rojas <joseantoniorojasl02@gmail.com>
2024-01-30 21:47:44 -0400

    📄 Added Footer

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
+export { Footer } from './footer/Footer';
 export { PageNotFound } from './not-found/PageNotFound';
 export { Sidebar } from './sidebar/Sidebar';
 export { Title } from './title/Title';
